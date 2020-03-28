c2d066045d4cfc0ba456573dff5fb0c4eb1ea348
Abay <abayibrayev7@gmail.com>
2020-03-28 18:23:10 +0000

    added initial app structure

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on the port 3000!");
+});
