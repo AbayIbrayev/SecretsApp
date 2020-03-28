083354c85de1a8771a9fe398adc4e23e5ceee00c
Abay <abayibrayev7@gmail.com>
2020-03-28 18:39:54 +0000

    added lvl1 authentication

--- app.js
+++ app.js
@@ -14,16 +14,63 @@ app.use(
 );
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.render("home");
 });
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-app.get("/register", (req, res) => {
-  res.render("register");
-});
+
+app.route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+    const username = req.body.username,
+      password = req.body.password;
+
+    User.findOne({
+      email: username
+    }, (err, foundUser) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          if (foundUser.password === password) {
+            res.render("secrets");
+          }
+        }
+      }
+    })
+  });
+
+app.route("/register")
+  .get((req, res) => {
+    res.render("register");
+  })
+  .post((req, res) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password
+    });
+
+    newUser.save(err => {
+      if (!err) {
+        res.render("secrets");
+      } else {
+        console.log(err);
+      }
+    });
+  });
 
 app.listen(3000, function () {
   console.log("Server started on the port 3000!");
